1df0541e1ebb01fa4e65c76c57b13ec0402b61ef
mirazfaruk <mirazfaruk@gmail.com>
2024-06-25 12:32:46 +0600

    third commit

---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,9 @@
+const Users = () => {
+    return (
+        <div>
+            <h2>Our Users</h2>
+        </div>
+    );
+};
+
+export default Users;
--- src/main.jsx
+++ src/main.jsx
@@ -9,19 +9,24 @@ import {
 import Home from './components/Home/Home.jsx';
 import About from './components/About/About.jsx';
 import Contact from './components/Contact/Contact.jsx';
+import Users from './components/Users/Users.jsx';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Home></Home>,
     children: [
       {
-        path: "/about",
+        path: '/about',
         element: <About></About>
       },
       {
-        path: "/contact",
+        path: '/contact',
         element: <Contact></Contact>
+      },
+      {
+        path: '/users',
+        element: <Users></Users>
       }
     ]
   },
