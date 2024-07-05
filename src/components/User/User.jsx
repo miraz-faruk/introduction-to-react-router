4d8eddc1be009f10e1b0541cd57fa854b714f07f
mirazfaruk <mirazfaruk@gmail.com>
2024-07-05 16:19:17 +0600

    added dynamic user details

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,5 +1,7 @@
-const User = ({user}) => {
-    const {id, name, email, phone} = user;
+import { Link } from "react-router-dom";
+
+const User = ({ user }) => {
+    const { id, name, email, phone } = user;
     const userStyle = {
         border: '2px solid red',
         padding: '5px',
@@ -10,6 +12,10 @@ const User = ({user}) => {
             <h2>{name}</h2>
             <h2>E-mail: {email}</h2>
             <h2>Phone: {phone}</h2>
+            <Link to={`/user/${id}`}>Show Details</Link>
+            <Link to={`/user/${id}`}>
+                <button>Click me</button>
+            </Link>
         </div>
     );
 };
